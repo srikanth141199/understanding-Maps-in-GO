f74bbe66c032c647e75f1a94ad741800a750fb27
Srikanth K <kolleparasrikanth@gmail.com>
2024-11-21 19:58:56 +0530

    diff ways to create a map

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	color := map[string]string{
+		"red":   "#ff0000",
+		"green": "4bf745",
+	}
+
+	fmt.Println(color)
+	//diff ways to create a map
+	var color1 map[string]string
+	color2 := make(map[string]string)
+	fmt.Println(color1)
+	fmt.Println(color2)
+}
